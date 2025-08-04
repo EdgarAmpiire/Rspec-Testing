a86c1716a094369a1ed360b9f013d4a993a3ee31
Edgar Ampiire <eampiire@gmail.com.com>
2025-08-04 15:28:24 +0300

    ruby tests

--- ruby_testing/spec/calculator_spec.rb
+++ ruby_testing/spec/calculator_spec.rb
@@ -12,4 +12,16 @@ describe Calculator do
       expect(calculator.add(2,7,5)).to eql(14)
     end
   end
+
+  describe "#Product" do
+    it "returns the product of two numbers" do
+      calculator = Calculator.new
+      expect(calculator.product(5,5)).to eql(25)
+    end
+
+    it "returns the product of more than two numbers" do
+      calculator = Calculator.new
+      expect(calculator.product(2,5,8)).to eql(80)
+    end
+  end
 end
